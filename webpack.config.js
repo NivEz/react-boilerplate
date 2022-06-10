a991191a06cdb3abc34ebd04c849996d3b72f59b
NivEz <niv.cyber@gmail.com>
2022-06-10 15:46:23 +0300

    added cross-env and webpack improvement with classNames

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,61 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
     mode: mode,
-    devtool: mode === 'development' && 'source-map' || '',
-    entry: "./src/index.js",
+    devtool: mode === 'development' && 'source-map',
+    entry: './src/index.js',
     output: {
-        filename: "bundle.[fullhash].js",
-        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.[fullhash].js',
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html",
+            template: './src/index.html',
         }),
     ],
     resolve: {
-        modules: [__dirname, "src", "node_modules"],
-        extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+        modules: [__dirname, 'src', 'node_modules'],
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
     },
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: require.resolve("babel-loader"),
+                loader: require.resolve('babel-loader'),
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: require.resolve("babel-loader"),
+                loader: require.resolve('babel-loader'),
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: mode === 'development' ? '[folder]__[local]' : '[hash:base64]',
+                            },
+                        },
+                    },
+                ],
             },
             {
                 test: /\.png|svg|jpg|gif$/,
-                use: ["file-loader"],
+                use: ['file-loader'],
             },
         ],
     },
 
     devServer: {
-        port: 5000
-    }
+        port: 4000,
+    },
 };
