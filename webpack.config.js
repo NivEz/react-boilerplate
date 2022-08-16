ff1a1c2e9a4922d74dd241c8c8fd28ed80a3a569
NivEz <niv.cyber@gmail.com>
2022-08-17 01:28:20 +0300

    added dotenv to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,67 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+require('dotenv').config();
 
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
-    mode: mode,
-    devtool: mode === 'development' && 'source-map',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.[fullhash].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-    ],
-    resolve: {
-        modules: [__dirname, 'src', 'node_modules'],
-        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: require.resolve('babel-loader'),
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: require.resolve('babel-loader'),
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                localIdentName: mode === 'development' ? '[folder]__[local]' : '[hash:base64]',
-                            },
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.png|svg|jpg|gif$/,
-                use: ['file-loader'],
-            },
-        ],
-    },
+	mode: mode,
+	devtool: mode === 'development' && 'source-map',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.[fullhash].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+		new webpack.DefinePlugin({
+			'process.env': JSON.stringify(process.env),
+		}),
+	],
+	resolve: {
+		modules: [__dirname, 'src', 'node_modules'],
+		extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: require.resolve('babel-loader'),
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: require.resolve('babel-loader'),
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							modules: {
+								localIdentName:
+									mode === 'development' ? '[folder]__[local]' : '[hash:base64]',
+							},
+						},
+					},
+				],
+			},
+			{
+				test: /\.png|svg|jpg|gif$/,
+				use: ['file-loader'],
+			},
+		],
+	},
 
-    devServer: {
-        port: 4000,
-    },
+	devServer: {
+		port: 3000,
+	},
 };
